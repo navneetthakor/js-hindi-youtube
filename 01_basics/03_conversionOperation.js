5951a02e0b7f61f5b7f3e99b5312ef32beafa47c
NAVNEETKUMAR RAMANBHAI THAKOR <126951586+navneetthakor@users.noreply.github.com>
2024-05-01 17:49:29 +0530

    Update 03_conversionOperation.js -> added more examples
    added more examples to clear some hidden stuffs.

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,15 +11,18 @@ let valueInNumber = Number(score)
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
+// "3 " or " 3" or " 3 "=> 3;  "3 3" => NaN
+//null => 0; undefined => NaN
 
 let isLoggedIn = "hitesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false
+// non-zero => true; 0 => false
 // "" => false
 // "hitesh" => true
+// null or undefined => false
 
 let someNumber = 33
 
@@ -46,15 +49,18 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
+// console.log("1" + 2); => 12
 // console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2); => 122
+// console.log(1 + 2 + "2"); => 32
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); => 1
+// console.log(+""); => 0
+// console.log(5-"10"); => -5
+// console.log(5-"10" + 5); => 0
+// console.log("nk"-"10"); => NaN
 
 let num1, num2, num3
 
