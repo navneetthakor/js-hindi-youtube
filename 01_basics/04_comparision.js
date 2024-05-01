053677f9bb14fd4df50ef4e04cf58dabe5d1fb47
NAVNEETKUMAR RAMANBHAI THAKOR <126951586+navneetthakor@users.noreply.github.com>
2024-05-01 17:58:22 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -10,12 +10,12 @@
 
 console.log(null > 0);
 console.log(null == 0);
-console.log(null >= 0);
+console.log(null >= 0); // null with >= or <= true else false
 
 console.log(undefined == 0);
 console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined < 0); // undefined alwas false
 
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2); // provides type safety; true only if type and value both are same; no type conversion before comparision
